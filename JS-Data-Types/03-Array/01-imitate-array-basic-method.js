1d43db58c36cf46722f02454726a7e4466447378
xusp <xspstudyemail@163.com>
2021-08-25 14:42:31 +0800

    手动实现数组方法

---
+++ JS-Data-Types/03-Array/01-imitate-array-basic-method.js
@@ -0,0 +1,119 @@
+// 1. 栈方法
+/**
+ * 
+ * push(element1, ..., elementN)：将一个或多个元素添加到数组的末尾，并返回该数组的新长度
+ * pop()：从数组中删除最后一个元素，并返回该元素的值
+ */
+// 2. 队列方法
+/**
+ * 
+ * push() + shift()：从数组中删除第一个元素，并返回该元素的值
+ * 或
+ * pop() + unshift(element1, ..., elementN)：将一个或多个元素添加到数组的开头，并返回该数组的新长度
+*/
+// 3. 万能方法
+// splice(start[, deleteCount[, item1[, item2[, ...]]]])：从 start 开始，删除 deleteCount 个元素
+// （若省略 deleteCount 则删除从 start 开始的剩余所有元素），并从原地补充新增元素，最后返回被删元素组成的数组
+
+
+Array.prototype._push = function (...endAdds) {
+    // 1. 把 endAdds 逐一添加到原数组后面
+    const startLen = this.length
+    for (let i = 0; i < endAdds.length; i++) {
+        this[startLen + i] = endAdds[i]
+    }
+    // 2. 返回添加后的数组长度
+    return this.length
+}
+
+Array.prototype._pop = function () {
+    // 1. 暂存原数组最后一项
+    const end = this[this.length - 1]
+    // 2. 删除最后一项
+    this[this.length - 1] = null
+    this.length = this.length - 1
+    // 3. 返回一开始暂存的最后一项
+    return end
+}
+
+Array.prototype._unshift = function (...startAdds) {
+    // 1. 把原数组的元素逐一后移 startAdds.length 个位置
+    const startAddsLen = startAdds.length
+    this.length = this.length + startAddsLen
+    for (let i = this.length - 1; i >= startAddsLen; i--) {
+        this[i] = this[i - startAddsLen]
+    }
+    // 2. 把 startAdds 逐一添加到原数组前面
+    for (let i = 0; i < startAddsLen; i++) {
+        this[i] = startAdds[i]
+    }
+    // 3. 返回添加后的数组长度
+    return this.length
+}
+
+Array.prototype._shift = function () {
+    // 1. 暂存原数组第一项
+    const start = this[0]
+    // 2. 从第二项开始，把原数组每一项往前移动一个单位
+    for (let i = 0; i < this.length - 1; i++) {
+        this[i] = this[i + 1]
+    }
+    this.length = this.length - 1
+    // 3. 返回一开始暂存的第一项
+    return start
+}
+
+Array.prototype._splice = function (startIndex, delCount, ...adds) {
+    const initialLen = this.length
+
+    // 1. 缓存被删元素, 并给它们打上标记
+    // 1.1 当没传入 delCount 时，delCount 为原数组从 startIndex 开始的剩余元素 
+    delCount = delCount || initialLen - startIndex
+    const delArr = []
+    for (let i = 0; i < delCount; i++) {
+        // 1.2 当删除的元素超过原数组的范围，剔除超出部分的 undefined 元素
+        if (this[startIndex + i] !== undefined) {
+            delArr[i] = this[startIndex + i]
+            // 1.3 被删除元素在原数组的位置打上标记
+            this[startIndex + i] = 'DELETE_ITEM'
+        }
+    }
+
+    // 2. 把 adds 从 startIndex 开始，添加到原数组, 再把原数组从 startIndex 开始后的剩余元素进行移动
+    const addsLen = adds.length
+    const leftLen = initialLen - delCount - startIndex
+    // 2.1 如果原数组没有剩余元素，只需把 adds 从 startIndex 开始逐一添加到原数组，而且，数组的长度变成 startIndex + addsLen
+    if (leftLen <= 0) {
+        for (let i = 0; i < addsLen; i++) {
+            this[startIndex + i] = adds[i]
+        }
+        this.length = startIndex + addsLen
+    } else {
+        // 2.2 如果原数组有剩余元素, 把 adds 从 startIndex 开始逐一添加到原数组后，再把剩余元素依次移动 addsLen - delCount 个位置
+        // 而且，数组的长度变成 initialLen - delCount + addsLen
+        
+        // 2.2.1 缓存剩余元素
+        const leftArr = []
+        for (let i = 0; i < leftLen; i++) {
+            leftArr[i] = this[startIndex + delCount + i]
+        }
+        // 2.2.2 增添增加元素
+        for (let i = 0; i < addsLen; i++) {
+            this[startIndex + i] = adds[i]
+        }
+        // 2.2.3 补上剩余元素
+        for (let i = 0; i < leftArr.length; i++) {
+            this[startIndex + addsLen + i] = leftArr[i]
+        }
+        // 2.2.4 最后数组的长度变成 initialLen - delCount + addsLen
+        this.length = initialLen - delCount + addsLen
+        // 2.2.5 释放 leftArr
+        leftArr.length = 0
+    }
+
+    // 3. 返回被删元素组成的数组
+    return delArr
+}
+const arr = [1,2,3,4,5,6]
+const res = arr._splice(2, 3)
+console.log('res', res, arr)
---
+++ JS-Data-Types/03-Array/02-imitate-array-higher-method.js
@@ -0,0 +1,221 @@
+// 1. 高级函数：函数的参数是函数或返回值是函数
+
+// 2. 数组中的高阶函数
+/**
+ * 
+ * 2.1 排序方法：sort([compareFunction])：如果省略 compareFunction，元素按照转换为的字符串的各个字符的 Unicode 位点进行排序
+ * 2.2 搜索方法
+ * find(callback(element[, index[, array]])[, thisArg])：返回数组中满足 callback 的第一个元素的值。否则返回 undefined
+ * findIndex(callback(element[, index[, array]])[, thisArg])：返回数组中满足 callback 的第一个元素的索引。否则返回 -1
+ * 2.3 迭代方法
+ * every(callback(element[, index[, array]])[, thisArg])：测试一个数组内的所有元素是否都能通过 callback 的测试。返回一个布尔值
+ * some(callback(element[, index[, array]])[, thisArg]): 测试数组中是不是至少有1个元素通过 callback 的测试。返回一个布尔值
+ * filter(callback(element[, index[, array]])[, thisArg])：返回数组的元素满足 callback 测试组成的数组
+ * map(callback(element[, index[, array]])[, thisArg])：返回数组中的每个元素调用 callback 后的新数组
+ * forEach(callback(element[, index[, array]])[, thisArg])：对数组中的每个元素执行 calllback
+ * 2.4 归并方法
+ * reduce(callback(accumulator, currentValue[, index[, array]])[, initialValue])：对数组中的每个元素（从左往右）执行 callback 函数，将其结果汇总为单个返回
+ * reduceRight(callback(accumulator, currentValue[, index[, array]])[, initialValue])：对数组中的每个元素（从右往左）执行 callback 函数，将其结果汇总为单个返回
+ */
+
+const { valid_array, valid_function, to_object, get_length } = require('../../utils/index')
+
+
+// 手动实现 sort
+
+// 手动实现 find
+Array.prototype._find = function (callback, thisArg) {
+    // 1. 数组异常，函数异常校验
+    valid_array(this, '_find')
+    valid_function(callback)
+    // 2. 数组转对象
+    const obj = to_object(this)
+    const len = get_length(this)
+    // 3. 迭代数组，返回数组中满足 callback 的第一个元素的值
+    for (let i = 0; i < len; i++) {
+        if (i in obj) {
+            const item = obj[i]
+            if (callback.call(thisArg, item, i, obj)) {
+                return item
+            }
+        }
+    }
+    // 4. 找不到返回 undefined
+    return undefined
+}
+
+// 手动实现 findIndex
+Array.prototype._findIndex = function (callback, thisArg) {
+    // 1. 数组异常，函数异常校验
+    valid_array(this, '_findIndex')
+    valid_function(callback)
+    // 2. 数组转对象
+    const obj = to_object(this)
+    const len = get_length(this)
+    // 3. 迭代数组，返回数组中满足 callback 的第一个元素的索引
+    for (let i = 0; i < len; i++) {
+        if (i in obj) {
+            const item = obj[i]
+            if (callback.call(thisArg, item, i, obj)) {
+                return i
+            }
+        }
+    }
+    // 4. 找不到返回 -1
+    return -1
+}
+
+// 手动实现 every
+Array.prototype._every = function (callback, thisArg) {
+    // 1. 数组异常，函数异常校验
+    valid_array(this, '_every')
+    valid_function(callback)
+    // 2. 数组转对象
+    const obj = to_object(this)
+    const len = get_length(this)
+    // 3. 迭代数组，测试是否每一个元素都能通过 callback 测试
+    for (let i = 0; i < len; i++) {
+        if (i in obj) {
+            const item = obj[i]
+            // 4. 只要有一个不满足，则返回false
+            if (!callback.call(thisArg, item, i, obj)) {
+                return false
+            }
+        }
+    }
+    // 5. 全都满足返回 true
+    return true
+}
+
+// 手动实现 some
+Array.prototype._some = function (callback, thisArg) {
+    // 1. 数组异常，函数异常校验
+    valid_array(this, '_some')
+    valid_function(callback)
+    // 2. 数组转对象
+    const obj = to_object(this)
+    const len = get_length(this)
+    // 3. 迭代数组，测试是否有元素都能通过 callback 测试
+    for (let i = 0; i < len; i++) {
+        if (i in obj) {
+            const item = obj[i]
+            // 4. 只要有一个满足，则返回 true
+            if (callback.call(thisArg, item, i, obj)) {
+                return true
+            }
+        }
+    }
+    // 5. 全都不满足返回 false
+    return false
+}
+
+// 手动实现 filter
+Array.prototype._filter = function (callback, thisArg) {
+    // 1. 数组异常，函数异常校验
+    valid_array(this, '_filter')
+    valid_function(callback)
+    // 2. 数组转对象
+    const obj = to_object(this)
+    const len = get_length(this)
+    const filterArr = []
+    let filterIndex = 0
+    // 3. 迭代数组，把满足 callback 的元素放入 filterArr
+    for (let i = 0; i < len; i++) {
+        if (i in obj) {
+            const item = obj[i]
+            if (callback.call(thisArg, item, i, obj)) {
+                filterArr[filterIndex++] = item
+            }
+        }
+    }
+    // 4. 返回过滤后的数组
+    return filterArr
+}
+
+// 手动实现 map
+Array.prototype._map = function (callback, thisArg) {
+    // 1. 数组异常，函数异常校验
+    valid_array(this, '_map')
+    valid_function(callback)
+    // 2. 数组转对象
+    const obj = to_object(this)
+    const len = get_length(this)
+    const mapArr = []
+    // 3. 迭代数组，对数组的每个元素执行 callback 方法
+    for (let i = 0; i < len; i++) {
+        if (i in obj) {
+            mapArr[i] = callback.call(thisArg, obj[i], i, obj)
+        }
+    }
+    // 4. 返回执行后的数组
+    return mapArr
+}
+
+// 手动实现 reduce
+Array.prototype._reduce = function (callback, initialValue) {
+    // 1. 数组异常校验
+    valid_array(this, '_reduce')
+    valid_function(callback)
+    // 2. 数组转对象
+    const obj = to_object(this)
+    const len = get_length(this)
+    let accumulator = initialValue
+    let i = 0
+    // 3. 如果没有传入初值值 initialValue，将数值第一个有效值赋值给 accumulator
+    if (initialValue === undefined) {
+        for (; i < len; i++) {
+            if (i in obj) {
+                accumulator = obj[i]
+                i++
+                break
+            }
+        }
+    }
+    // 4. 如果数组全为空
+    if (i === len && accumulator === undefined) {
+        throw new Error('Each element of the array is empty')
+    }
+    // 5. 从头到尾迭代数组剩余项，不断对 accumulator 进行 callback 操作，并重新赋值
+    for (; i < len; i++) {
+        if (i in obj) {
+            accumulator = callback.call(undefined, accumulator, obj[i], i, obj)
+        }
+    }
+    // 6. 返回累加结果
+    return accumulator
+}
+
+// 手动实现 reduceRight
+Array.prototype._reduceRight = function (callback, initialValue) {
+    // 1. 数组异常校验
+    valid_array(this, '_reduceRight')
+    valid_function(callback)
+    // 2. 数组转对象
+    const obj = to_object(this)
+    const len = get_length(this)
+    let accumulator = initialValue
+    let i = len - 1
+    // 3. 如果没有传入初值值 initialValue，将数值倒数第一个有效值赋值给 accumulator
+    if (initialValue === undefined) {
+        for (; i >= 0; i--) {
+            if (i in obj) {
+                accumulator = obj[i]
+                i--
+                break
+            }
+        }
+    }
+
+    // 4. 如果数组全为空
+    if (i === 0 && accumulator === undefined) {
+        throw new Error('Each element of the array is empty')
+    }
+    // 5. 从尾到头迭代数组剩余项，不断对 accumulator 进行 callback 操作，并重新赋值
+    for (; i >= 0; i--) {
+        if (i in obj) {
+            accumulator = callback.call(undefined, accumulator, obj[i], i, obj)
+        }
+    }
+    // 6. 返回累加结果
+    return accumulator
+}
